ca3d897d75e92a2222f0b57313c667c7d3dba0a3
Chris Achard <chrisachard@gmail.com>
2018-04-07 15:36:54 -0400

    Null and undefined - lesson 02

---
+++ 02.js
@@ -0,0 +1,35 @@
+undefined
+// undefined
+
+typeof undefined
+// 'undefined'
+
+typeof myVar
+// 'undefined'
+
+null
+// null
+
+myVariable = null
+// null
+
+typeof null
+// 'object'
+
+1 + null
+// 1
+
+1 + undefined
+// NaN
+
+"test " + null
+// 'test null'
+
+"test " + undefined
+// 'test undefined'
+
+Boolean(null)
+// false
+
+Boolean(undefined)
+//false
