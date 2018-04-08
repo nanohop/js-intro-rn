04f4503cd41b52cf95f22d03deb62c68b3bb1bbf
Chris Achard <chrisachard@gmail.com>
2018-04-07 23:19:19 -0400

    Comparing values

---
+++ 05.js
@@ -0,0 +1,59 @@
+5 == 5
+// true
+
+5 == "5"
+// true
+
+1 == true
+// true
+
+0 == false
+// true
+
+5 === 5
+// true
+
+5 === "5"
+// false
+
+5 === Number("5")
+// true
+
+5 != 5
+// false
+
+5 != "5"
+// false
+
+5 !== 5
+// false
+
+5 !== "5"
+// true
+
+5 > 5
+// false
+
+5 > 6
+// false
+
+5 > 4
+// true
+
+"A" < "a"
+// true
+
+"a" < "b"
+// true
+
+"a" > 1
+// false
+
+"a" < 1
+// false
+
+1 < "5"
+// true
+
+1 > "5"
+// false
