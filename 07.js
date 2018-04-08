cf658f367987bd04d2124684b518ced097cfa066
Chris Achard <chrisachard@gmail.com>
2018-04-08 14:40:50 -0400

    Switch/case

---
+++ 07.js
@@ -0,0 +1,39 @@
+
+switch(5) {
+  case 1: {
+    
+  }
+}
+
+switch(5) {
+  case 1:
+    console.log("1")
+  case 5:
+  case 6:
+    console.log("5 or 6")
+  case 10:
+    console.log("10")
+}
+
+switch("5") {
+  case 1:
+    console.log("1")
+    break
+  case 5:
+  case 6:
+    console.log("5 or 6")
+    break
+  case 10:
+    console.log("10")
+    break
+  default:
+    console.log("no case matched")
+}
+
+switch(5) {
+  case 1: console.log("1"); break
+  case 5: console.log("5"); break
+  case 10: console.log("10"); break
+  default: console.log("no case matched")
+}
+
